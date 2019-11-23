ab60acbba4b26f817cbf619ddc3131593793fc95
Okusama <david.nuad75@live.fr>
2019-11-23 12:01:38 +0100

    Init

---
+++ kitsune_front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+    render(): React.ReactElement<React.JSXElementConstructor<any>> {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
+
+}
